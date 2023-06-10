06139091ab2555fb992a3246ded7327baaf3bc64
sigmaresearch100 <136125754+sigmaresearch100@users.noreply.github.com>
2023-06-10 10:14:50 +0800

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -3,17 +3,17 @@
 set -e
 
 # upgrade to C++ 17 and C++ 20
-# sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-# sudo apt-get update
-# sudo apt-get install -y gcc-10 gcc-10-base gcc-10-doc g++-10
-# sudo apt-get install -y libstdc++-10-dev libstdc++-10-doc
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install -y gcc-10 gcc-10-base gcc-10-doc g++-10
+sudo apt-get install -y libstdc++-10-dev libstdc++-10-doc
 
 Rscript -e "                                           \
   install.packages(                                    \
     'OpenMx',                                          \
     contriburl = contrib.url(                          \
       repos = 'https://openmx.ssri.psu.edu/software/', \
-      type = getOption('pkgType')                      \
+      type = 'source'                                  \
     ),                                                 \
     dependencies = NA,                                 \
     verbose = TRUE                                     \
