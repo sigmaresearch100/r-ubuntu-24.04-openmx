cda3fd7adebabf83d16c8de5d391a998ed28a821
sigmaresearch100 <136125754+sigmaresearch100@users.noreply.github.com>
2023-06-10 11:05:24 +0800

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -3,10 +3,10 @@
 set -e
 
 # upgrade to C++ 17 and C++ 20
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt-get update
-sudo apt-get install -y gcc-10 gcc-10-base gcc-10-doc g++-10
-sudo apt-get install -y libstdc++-10-dev libstdc++-10-doc
+# sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+# sudo apt-get update
+# sudo apt-get install -y gcc-10 gcc-10-base gcc-10-doc g++-10
+# sudo apt-get install -y libstdc++-10-dev libstdc++-10-doc
 
 Rscript -e "                                           \
   install.packages(                                    \
