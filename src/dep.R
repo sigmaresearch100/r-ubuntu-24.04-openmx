05761fcb0282ac9acc17f3a8a6a7f84b76649657
sigmaresearch100 <136125754+sigmaresearch100@users.noreply.github.com>
2023-06-10 09:49:24 +0800

    Create dep.R

---
+++ src/dep.R
@@ -0,0 +1,14 @@
+root <- rprojroot::is_rstudio_project
+dep <- read.csv(
+  root$find_file(
+    "src",
+    "dep-r.csv"
+  ),
+  head = FALSE
+)$V1
+for (i in seq_along(dep)) {
+  install.packages(
+    dep[i],
+    repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/jammy/latest")
+  )
+}
