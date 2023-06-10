8e66e6a1334ed74e854d416a8f1af5124a8106eb
sigmaresearch100 <136125754+sigmaresearch100@users.noreply.github.com>
2023-06-10 10:02:25 +0800

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+set -e
+
+Rscript -e "                                           \
+  install.packages(                                    \
+    'OpenMx',                                          \
+    contriburl = contrib.url(                          \
+      repos = 'https://openmx.ssri.psu.edu/software/', \
+      type = getOption('pkgType')                      \
+    ),                                                 \
+    dependencies = NA,                                 \
+    verbose = TRUE                                     \
+  );                                                   \
+  demo('BivariateCorrelation', package = 'OpenMx')     \
+"
