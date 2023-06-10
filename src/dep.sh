414d881c2f55d4d9ce589a5eccded7ac96cf3f56
sigmaresearch100 <136125754+sigmaresearch100@users.noreply.github.com>
2023-06-10 09:50:40 +0800

    Update dep.sh

--- src/dep.sh
+++ src/dep.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+set -e
+
+sudo apt-get update
+sudo apt-get install -y \
+    build-essential     \
+    cmake               \
+    libopenblas-dev     \
+    liblapack-dev       \
+    libarpack2-dev      \
+    libsuperlu-dev      \
+    libgsl-dev          \
+    libarmadillo-dev    \
+    libeigen3-dev
+    
