d0d2167e92f9cc0d4fc0c5e986ae908df63a918c
sigmaresearch100 <136125754+sigmaresearch100@users.noreply.github.com>
2023-07-05 00:27:58 +0800

    Find deps.

--- tools/dep.sh
+++ tools/dep.sh
@@ -3,13 +3,13 @@
 set -e
 
 apt-get update
-apt-get install -y   \
-    build-essential  \
-    cmake            \
-    libopenblas-dev  \
-    liblapack-dev    \
-    libarpack2-dev   \
-    libsuperlu-dev   \
-    libgsl-dev       \
-    libarmadillo-dev \
-    libeigen3-dev
+#apt-get install -y   \
+#    build-essential  \
+#    cmake            \
+#    libopenblas-dev  \
+#    liblapack-dev    \
+#    libarpack2-dev   \
+#    libsuperlu-dev   \
+#    libgsl-dev       \
+#    libarmadillo-dev \
+#    libeigen3-dev
